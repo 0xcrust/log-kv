9bccce718bec00d0aa0099e977f56f6f5b3583ea
Ademola <ademolafatoke@gmail.com>
2023-06-29 23:08:55 +0100

    feat: add compaction mechanism. all done!

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,31 @@
 pub mod err;
 mod op;
 
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 use std::fs::File;
 use std::io::prelude::*;
 
 pub use crate::err::{KvsError, Result};
 use crate::op::Op;
 
-use serde::{Deserialize, Serialize};
 use serde_json::Deserializer;
+
+/// The number of ops that can be performed before another log-compaction
+/// process needs to be done.
+const SIZE_LIMIT: usize = 10_000;
 
 /// The store.
 pub struct KvStore {
+    /// The path to the logfile.
+    fp: std::path::PathBuf,
     /// The handle to the logfile.
     fh: std::fs::File,
-    /// An index mapping a key to the file offset
-    /// of its last `set` value.
+    /// An index mapping a key to the file offset of its last `set` op.
     index: BTreeMap<String, u64>,
+    /// The number of operations that have been made since the store last
+    /// underwent compaction.
+    uncmp: usize,
 }
-
-#[derive(Serialize, Deserialize)]
-pub struct Log(Vec<Op>);
 
 impl KvStore {
     /// Set a key-value pair.
@@ -32,7 +36,11 @@ impl KvStore {
         self.index.insert(key, self.fh.stream_position()?);
         self.fh
             .write_all(serde_json::to_string_pretty(&op)?.as_bytes())?;
-        self.add_op(op)?;
+        self.uncmp += 1;
+
+        if self.needs_compaction() {
+            self.compact()?;
+        }
 
         Ok(())
     }
@@ -45,6 +53,11 @@ impl KvStore {
                 let op = Op::rm(key);
                 self.fh
                     .write_all(serde_json::to_string_pretty(&op)?.as_bytes())?;
+                self.uncmp += 1;
+
+                if self.needs_compaction() {
+                    self.compact()?;
+                }
                 Ok(())
             }
             None => {
@@ -79,25 +92,21 @@ impl KvStore {
     /// Open the KvStore at a given path.
     pub fn open(path: impl Into<std::path::PathBuf>) -> Result<KvStore> {
         let mut path: std::path::PathBuf = path.into();
-        path.push("logs.json");
+        path.push("db-lock.log");
 
         let fh = File::options()
             .create(true)
             .read(true)
             .write(true)
-            .open(path)?;
+            .open(path.clone())?;
 
         let mut stream = Deserializer::from_reader(&fh).into_iter::<Op>();
         let mut index = BTreeMap::new();
 
         let mut offset = stream.byte_offset();
-        //println!("start offset: {:?}", offset);
         while let Some(op) = stream.next() {
-            //println!("item: {:?}", item);
-            //println!("offset: {:?}", offset);
             match op? {
                 Op::Set { key, .. } => {
-                    //test_vec.push(op.clone());
                     index.insert(key, offset as u64);
                 }
                 Op::Rm { key } => {
@@ -106,14 +115,42 @@ impl KvStore {
             }
             offset = stream.byte_offset();
         }
-        //println!("ops gotten: {:?}", test_vec);
 
-        Ok(KvStore { fh, index })
+        Ok(KvStore {
+            fp: path,
+            fh,
+            index,
+            uncmp: 0,
+        })
     }
 
-    fn add_op(&mut self, op: Op) -> Result<()> {
-        self.fh
-            .write_all(serde_json::to_string_pretty(&op)?.as_bytes())?;
+    fn compact(&mut self) -> Result<()> {
+        self.fh.rewind()?;
+
+        let mut stream = Deserializer::from_reader(&mut self.fh).into_iter::<Op>();
+        let mut keep = HashMap::new();
+        while let Some(op) = stream.next() {
+            let op = op?;
+            match op.clone() {
+                Op::Set { key, .. } => _ = _ = keep.insert(key, op),
+                Op::Rm { key } => _ = keep.remove(&key),
+            }
+        }
+
+        let mut nf = File::options()
+            .create(true)
+            .truncate(true)
+            .read(true)
+            .write(true)
+            .open(self.fp.clone())?;
+
+        for (_, op) in keep {
+            nf.write_all(serde_json::to_string_pretty(&op)?.as_bytes())?;
+        }
         Ok(())
+    }
+
+    fn needs_compaction(&self) -> bool {
+        self.uncmp == SIZE_LIMIT
     }
 }
